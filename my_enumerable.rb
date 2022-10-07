c603f690c4885198b6068957bde818c26e3b2705
basitali111 <allahbadshah148@gmail.com>
2022-10-07 19:53:56 +0500

    adding code

---
+++ my_enumerable.rb
@@ -0,0 +1,13 @@
+module Enumerable
+  def all(list)
+    list.all? { |num| num < 5 }
+  end
+
+  def any(list)
+    list.any? { |num| num == 5 }
+  end
+
+  def filter(list)
+    list.filter(&:even?)
+  end
+end
--- my_list.rb
+++ my_list.rb
@@ -1,1 +1,30 @@
+require_relative "my_enumerable.rb"
 
+class Mylist
+  include Enumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  attr_reader :list
+
+  def each
+    yield @list
+  end
+end
+list_of_nums = Mylist.new(1, 2, 3, 4)
+
+print "#{list_of_nums.list}\n" # [1, 2, 3, 4]
+
+list_of_nums.each do |num|
+  puts num
+end
+# 1
+# 2
+# 3
+# 4
+
+puts list_of_nums.all(list_of_nums.list) # true
+puts list_of_nums.any(list_of_nums.list) # false
+print list_of_nums.filter(list_of_nums.list) # [2, 4]
